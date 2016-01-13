643f740d97945d639532653d02feeedd801d9d7e
Ichiko Tanabe <ichiko.tanabe@gmail.com>
2016-01-13 21:56:49 +0000

    2 Counting the Number of Characters

---
+++ python/02/count_chars.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+class CharCounter(object):
+    def __init__(self, input_string):
+        self.instr = input_string
+
+    def get_input_string(self):
+        return self.instr
+
+    def count_chars(self):
+        return len(self.instr)
+
+
+def main():
+    cc = CharCounter(raw_input("What is the input string? "))
+    ccount = cc.count_chars()
+    plural_suffix = '' if ccount == 1 else 's'
+
+    print '{} has {} character{}.'.format(
+        cc.get_input_string(),
+        ccount,
+        plural_suffix
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ python/02/count_chars_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from count_chars import CharCounter
+
+
+class TestCharCounter(unittest.TestCase):
+    def test_happy_path(self):
+        cc = CharCounter('Ichiko Tanabe')
+        self.assertEqual(13, cc.count_chars())
+
+    def test_0_chars(self):
+        cc = CharCounter('')
+        self.assertEqual(0, cc.count_chars())
+
+    def test_1_space(self):
+        cc = CharCounter(' ')
+        self.assertEqual(1, cc.count_chars())
+
+    def test_1_char(self):
+        cc = CharCounter('')
+        self.assertEqual(0, cc.count_chars())
+
+    def test_non_ascii_western_char(self):
+        cc = CharCounter(u"á")
+        self.assertEqual(1, cc.count_chars())
+
+    def test_double_byte_char(self):
+        cc = CharCounter(u"市")
+        self.assertEqual(1, cc.count_chars())
