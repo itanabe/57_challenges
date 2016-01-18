a93b6a9dd8d9425c5d4c0840044506bd7a0851c3
Ichiko Tanabe <ichiko.tanabe@gmail.com>
2016-01-18 23:05:00 +0000

    s/person/author/g

--- python/03/print_quote.py
+++ python/03/print_quote.py
@@ -1,9 +1,9 @@
 quote = ''
-person = ''
+author = ''
 
 while len(quote) == 0:
     quote = raw_input("What is the quote? ").strip()
 while len(author) == 0:
-    person = raw_input("Who said it? ").strip()
+    author = raw_input("Who said it? ").strip()
 
-print person + " says, \"" + quote + "\""
+print author + " says, \"" + quote + "\""
