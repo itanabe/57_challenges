3af2bbc604d1eee3baf646cb4fc117e6f85d7510
itanabe <i>
2016-02-02 21:32:32 +0000

    Exercise 5: Challenge - Revise the program to ensure that inputs are entered as numeric values. Don't allow the user to proceed if the value entered is not numeric.

--- python/05/simple_math.py
+++ python/05/simple_math.py
@@ -1,7 +1,22 @@
-first = raw_input("What is the first number? ")
-second = raw_input("What is the second number? ")
-n1 = int(first)
-n2 = int(second)
+first = None
+second = None
+n1 = None
+n2 = None
+
+while (n1 is None):
+    first = raw_input("What is the first number? ")
+    try:
+        n1 = int(first)
+    except:
+        continue
+
+while (n2 is None):
+    second = raw_input("What is the second number? ")
+    try:
+        n2 = int(second)
+    except:
+        continue
+
 template = '{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}'
 print template.format(first, second, n1 + n2,
                       first, second, n1 - n2,
