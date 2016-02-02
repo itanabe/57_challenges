ccf62e65406078e9a74d51ea336c7c5a315deafe
itanabe <i>
2016-02-02 21:23:27 +0000

    Exercise 5: Apply constraints

--- python/05/simple_math.py
+++ python/05/simple_math.py
@@ -1,6 +1,9 @@
 first = raw_input("What is the first number? ")
-second = raw_input("What is the second nnumber? ")
-print '{} + {} = {}'.format(first, second, int(first) + int(second))
-print '{} - {} = {}'.format(first, second, int(first) - int(second))
-print '{} * {} = {}'.format(first, second, int(first) * int(second))
-print '{} / {} = {}'.format(first, second, int(first) / int(second))
+second = raw_input("What is the second number? ")
+n1 = int(first)
+n2 = int(second)
+template = '{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}'
+print template.format(first, second, n1 + n2,
+                      first, second, n1 - n2,
+                      first, second, n1 * n2,
+                      first, second, n1 / n2)
