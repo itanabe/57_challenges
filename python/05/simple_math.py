27ebcdfc7eed7d44a7076e438e88e62f1250d86c
itanabe <i>
2016-02-02 21:38:08 +0000

    Exercise 5: Challenge - Don't allow the user to enter a negative number.

--- python/05/simple_math.py
+++ python/05/simple_math.py
@@ -7,6 +7,10 @@ while (n1 is None):
     first = raw_input("What is the first number? ")
     try:
         n1 = int(first)
+        if n1 < 0:
+            n1 = None
+            print ("Please enter non-negative number")
+            continue
     except:
         continue
 
@@ -14,6 +18,10 @@ while (n2 is None):
     second = raw_input("What is the second number? ")
     try:
         n2 = int(second)
+        if n1 < 0:
+            n2 = None
+            print ("Please enter non-negative number")
+            continue
     except:
         continue
 
