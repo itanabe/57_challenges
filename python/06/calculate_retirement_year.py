0e06443280dbaab72a3b7530cccf7834de3ecf14
itanabe <i>
2016-02-10 21:19:25 +0000

    Exercise 6: Retirement Calculator

---
+++ python/06/calculate_retirement_year.py
@@ -0,0 +1,25 @@
+import datetime
+
+def get_current_age():
+    age = raw_input("What is your current age? ")
+    return int(age)
+
+def get_retirement_age():
+    age = raw_input("At what age would you like to retire? ")
+    return int(age)
+
+def calculate_retirement_year(current_age, retirement_age):
+    current_year = datetime.datetime.now().year
+    how_many_years_left = retirement_age - current_age
+    retirement_year = current_year + how_many_years_left
+    print "You have {} years left until you can retire.".format(how_many_years_left)
+    print "It's {}, so you ca retire in {}.".format(str(current_year), str(current_year + how_many_years_left))
+    return retirement_year
+
+def main():
+    current_age = get_current_age()
+    retirement_age = get_retirement_age()
+    calculate_retirement_year(current_age, retirement_age)
+
+if __name__ == '__main__':
+    main()
