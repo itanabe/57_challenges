66585d1ab52409695b1c18c1f648cf7aeb1532af
itanabe <i>
2016-02-10 21:33:55 +0000

    Exercise 6: Challenge - Handle situations where the program returns a negative number by stating that the user can already retire.

--- python/06/calculate_retirement_year.py
+++ python/06/calculate_retirement_year.py
@@ -12,8 +12,14 @@ def calculate_retirement_year(current_age, retirement_age):
     current_year = datetime.datetime.now().year
     how_many_years_left = retirement_age - current_age
     retirement_year = current_year + how_many_years_left
-    print "You have {} years left until you can retire.".format(how_many_years_left)
-    print "It's {}, so you ca retire in {}.".format(str(current_year), str(current_year + how_many_years_left))
+    if how_many_years_left > 0:
+        print "You have {} years left until you can retire.".format(how_many_years_left)
+        print "It's {}, so you can retire in {}.".format(str(current_year), str(current_year + how_many_years_left))
+    else:
+        if how_many_years_left == 0:
+            print "You can retire this year!"
+        else:
+            print "You should have retired {} years ago.".format(abs(how_many_years_left))
     return retirement_year
 
 def main():
